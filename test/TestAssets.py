55f8b91499259a4651196b615ee40c14df1a1cfe
AlexGeorgeLain <AlexGeorgeLain@users.noreply.github.com>
2022-01-28 20:17:48 +0000

    Restyled code

--- test/TestAssets.py
+++ test/TestAssets.py
@@ -3,13 +3,13 @@ import sys
 import unittest
 import pathlib as pl
 
-TMP_DEL = '×'
-PTH_DEL = '/'
+TMP_DEL = "×"
+PTH_DEL = "/"
 
 
 def clean_path(pth):
-    pth = pth.replace('/', TMP_DEL)
-    pth = pth.replace('\\', TMP_DEL)
+    pth = pth.replace("/", TMP_DEL)
+    pth = pth.replace("\\", TMP_DEL)
     return pth
 
 
@@ -28,10 +28,11 @@ def get_root_by_name(__file__, dir_name):
 def get_specific_parent_dir(__file__, dir_name):
     pth = clean_path(get_path(__file__))
     dir_name = clean_path(dir_name)
-    candidate = f'{TMP_DEL}{dir_name}{TMP_DEL}'
+    candidate = f"{TMP_DEL}{dir_name}{TMP_DEL}"
     if candidate in pth:
-        pth = (pth.split(candidate)[0] + TMP_DEL +
-               dir_name).replace(TMP_DEL * 2, TMP_DEL)
+        pth = (pth.split(candidate)[0] + TMP_DEL + dir_name).replace(
+            TMP_DEL * 2, TMP_DEL
+        )
         return pth.replace(TMP_DEL, PTH_DEL)
     return None
 
@@ -54,33 +55,33 @@ class TestCaseBase(unittest.TestCase):
 
 
 class ActualTest(TestCaseBase):
-    project_dir = get_root_by_name(__file__, 'sepm-development')
+    project_dir = get_root_by_name(__file__, "sepm-development")
 
     def test_audio(self):
-        audio = ['8-Bit March - Twin Musicom.mp3', "theme.wav"]
+        audio = ["8-Bit March - Twin Musicom.mp3", "theme.wav"]
         # test for audio assets
         for i in audio:
             p = ActualTest.project_dir + "/assets/audio/" + i
             TestCaseBase.assert_is_file(p)
 
     def test_cars(self):
-        cars = ['black-car.png', 'blue-car.png', 'green-car.png', 'red-car.png']
+        cars = ["black-car.png", "blue-car.png", "green-car.png", "red-car.png"]
         # test for car assets
         for i in cars:
             p = ActualTest.project_dir + "/assets/images/cars/" + i
             TestCaseBase.assert_is_file(p)
 
     def test_backgrounds(self):
-        backgrounds = ['dirt.png', 'grass.png', 'stone.png']
+        backgrounds = ["dirt.png", "grass.png", "stone.png"]
         # test for background assets
         for i in backgrounds:
             p = ActualTest.project_dir + "/assets/images/backgrounds/" + i
             TestCaseBase.assert_is_file(p)
 
     def test_tracks(self):
-        track_1 = ['track-1.png', 'track-1-border.png']
-        track_2 = ['track-2.png', 'track-2-border.png']
-        finish = ['finish.png']
+        track_1 = ["track-1.png", "track-1-border.png"]
+        track_2 = ["track-2.png", "track-2-border.png"]
+        finish = ["finish.png"]
         # test for track-1 assets
         for i in track_1:
             p = ActualTest.project_dir + "/assets/images/tracks/track-1/" + i
