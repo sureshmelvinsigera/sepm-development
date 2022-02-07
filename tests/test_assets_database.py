b136f38b9908da279608bc80ad9bcf701629074c
KieronHolmes <kieron@kbho.co.uk>
2022-02-07 22:31:48 +0000

    Improved test cases and seeder validation.

---
+++ tests/test_assets_database.py
@@ -0,0 +1,18 @@
+from tests.base.BaseTestCase import BaseTestCase
+from database.models import Car,Track
+from os.path import exists
+
+
+class TestDatabaseAssetsExist(BaseTestCase):
+    """Checks whether database assets exist in the current file structure."""
+    def test_if_track_files_exist_database(self):
+        """Test if tracks in the database have associated files"""
+        for item in Track.select():
+            self.assertTrue(exists(item.track_path), f"File {item.track_path} referenced in the Tracks model does not exist.")
+            self.assertTrue(exists(item.border_path), f"File {item.border_path} referenced in the Tracks model does not exist.")
+            self.assertTrue(exists(item.background_path), f"File {item.background_path} referenced in the Tracks model does not exist.")
+
+    def test_if_car_files_exist_database(self):
+        """Test if cars in the database have associated files"""
+        for item in Car.select():
+            self.assertTrue(exists(item.car_path), f"File {item.car_path} referenced in the Cars model does not exist.")
---
+++ tests/test_censorship.py
@@ -0,0 +1,11 @@
+from tests.base.BaseTestCase import BaseTestCase
+from game.utilities import censor_word
+
+
+class TestCensorship(BaseTestCase):
+
+    def test_if_censors_static_input_correctly(self):
+        """Test if the censorship function correctly amends text."""
+        self.assertEqual("f**k", censor_word("fuck"), "fuck is not correctly being censored to f**k.")
+        self.assertEqual("b******s", censor_word("bollocks"), "bollocks is not correctly being censored to b*****s.")
+        self.assertEqual("if", censor_word("if"), "A 2 character string has been incorrectly mutated")
