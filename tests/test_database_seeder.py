2ce368e193b407ba6f6516d8f7378125997075c6
KieronHolmes <KieronHolmes@users.noreply.github.com>
2022-02-07 22:32:46 +0000

    Fix styling

--- tests/test_database_seeder.py
+++ tests/test_database_seeder.py
@@ -8,9 +8,16 @@ class TestDatabaseSeeder(BaseTestCase):
 
     def test_database_seeder_files(self):
         """Test the database seeder files follow the correct format."""
-        allowed_models = ['CAR', 'TRACK', 'HIGHSCORE', 'PATH', 'PROFILE', 'PROFANITY']
+        allowed_models = ["CAR", "TRACK", "HIGHSCORE", "PATH", "PROFILE", "PROFANITY"]
         for import_file in (x for x in os.listdir("database") if x.endswith(".yaml")):
             with open(f"database/{import_file}") as f:
                 for item in yaml.safe_load(f):
-                    self.assertTrue(item.get("model"), f"The model directive does not exist in {import_file} seeder.")
-                    self.assertIn(item.get("model").upper(), allowed_models, f"{item.get('model')} is not a valid model type in the {import_file} seeder.")
+                    self.assertTrue(
+                        item.get("model"),
+                        f"The model directive does not exist in {import_file} seeder.",
+                    )
+                    self.assertIn(
+                        item.get("model").upper(),
+                        allowed_models,
+                        f"{item.get('model')} is not a valid model type in the {import_file} seeder.",
+                    )
